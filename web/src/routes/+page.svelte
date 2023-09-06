ab0076d390f155467b68d5cff1f25437bc211760
Ratimon <ratixoxo@gmail.com>
2023-09-06 07:46:10 +0700

    Refactor (./web): iterating lists

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -14,10 +14,9 @@
     isDropdownOpen = false
   }
 
-  const menuItems = ["About", "Base", "Blog"]
+  const menuItems = ["TokenA", "TokenB", "TokenC"]
 
 </script>
-
 
 <h1 class="text-2xl text-center font-bold underline">
   Demo for call data optimization
@@ -30,7 +29,6 @@
   </h2>
 
   <div class="flex flex-col max-w-2xl mx-auto py-2 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-
     <div class="flex flex-row justify-between h-8 bg-slate-400">
       <div class="mx-2">
         Amount to be added
@@ -41,7 +39,6 @@
     </div>
 
     <div class="flex flex-row justify-between h-12 px-4 bg-slate-400">
-
       <form method="POST">
         <label>
           <input
@@ -52,7 +49,6 @@
         </label>
       </form>
 
-
       <button class="bg-slate-400 hover:bg-slate-600 rounded-md border-2 border-pink-300">
         Max
       </button>
@@ -61,44 +57,21 @@
         <div class="rounded-md">
           Select Token
         </div>
-
         <div class="dropdown" on:focusout={handleDropdownFocusLoss}>
           <button class="btn top-2 bg-slate-400 px-1 py-1 rounded-md border-0 border-pink-300" on:click={handleDropdownClick} >
-          {#if isDropdownOpen}
-            <!-- <svg
-                fill="none"
-                class="inline-block h-6 w-6 stroke-current bg-slate-400 ">
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M6 18L18 6M6 6l12 12" />
-              </svg> -->
-
+            {#if isDropdownOpen}
               <MdKeyboardArrowDown />
             {:else}
-            <!-- <svg
-                fill="none"
-                class="inline-block h-6 w-6 stroke-current bg-slate-400 ">
-                <title>Open Dropdown</title>
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M4 6h16M4 12h16M4 18h16" />
-            </svg> -->
-            <MdFormatListBulleted/>
+              <MdFormatListBulleted/>
             {/if}
           </button>
-
           <ul class="dropdown-content menu bg-slate-400 w-44" style:visibility={isDropdownOpen ? 'visible' : 'hidden'}>
-            <li><button class="btn text-slate-300 bg-slate-400">Item 1</button></li>
-            <li><button class="btn text-slate-300 bg-slate-400">Item 2</button></li>
+            {#each menuItems as item}
+              <li><button class="btn text-slate-300 bg-slate-400">{item}</button></li>
+            {/each}
           </ul>
-          
         </div>
       </div>
-
     </div>
 
     <div class="basis-1/2 border-2 border-pink-300">
