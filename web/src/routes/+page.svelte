ee4eb039bcb15c99226f42fbcb473001287d9443
Ratimon <ratixoxo@gmail.com>
2023-09-06 09:43:24 +0700

    Refactor (./web): Import child and reusing the second token field

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -2,19 +2,13 @@
 
   import MdFormatListBulleted from 'svelte-icons/md/MdFormatListBulleted.svelte'
   import MdKeyboardArrowDown from 'svelte-icons/md/MdKeyboardArrowDown.svelte'
+  import TokenField from './TokenField.svelte';
 
-  let isDropdownOpen = false // default state
+  let isDropdownOpen1 = false
+  let isDropdownOpen2 = false
 
-  const handleDropdownClick = () => {
-    isDropdownOpen = !isDropdownOpen 
-  }
-
-  const handleDropdownFocusLoss = ({ relatedTarget, currentTarget }) => {
-    if (relatedTarget instanceof HTMLElement && currentTarget.contains(relatedTarget)) return 
-    isDropdownOpen = false
-  }
-
-  const menuItems = ["TokenA", "TokenB", "TokenC"]
+  let menuItems1 = ["TokenA", "TokenB", "TokenC"]
+  let menuItems2 = ["Token1", "Token2"]
 
 </script>
 
@@ -29,62 +23,9 @@
   </h2>
 
   <div class="flex flex-col max-w-2xl mx-auto py-2 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-  
-    <div class="flex flex-col">
-      <div class="flex flex-row justify-between h-8 bg-slate-400">
-        <div class="mx-2">
-          Amount to be added
-        </div>
-        <div class="mx-2">
-          Balance
-        </div>
-      </div>
 
-      <div class="flex flex-row justify-between h-12 px-4 bg-slate-400">
-        <form method="POST">
-          <label>
-            <input
-              name="description"
-              autocomplete="off"
-              class="text-slate-700"
-            />
-          </label>
-        </form>
-
-        <button class="bg-slate-400 hover:bg-slate-600 rounded-md border-2 border-pink-300">
-          Max
-        </button>
-
-        <div class="flex justify-between items-center rounded-md border-2 border-pink-300">
-          <div class="rounded-md">
-            Select Token
-          </div>
-          <div class="dropdown" on:focusout={handleDropdownFocusLoss}>
-            <button class="btn top-2 bg-slate-400 px-1 py-1 rounded-md border-0 border-pink-300" on:click={handleDropdownClick} >
-              {#if isDropdownOpen}
-                <MdKeyboardArrowDown />
-              {:else}
-                <MdFormatListBulleted/>
-              {/if}
-            </button>
-            <ul class="dropdown-content menu bg-slate-400 w-44" style:visibility={isDropdownOpen ? 'visible' : 'hidden'}>
-              {#each menuItems as item}
-                <li><button class="btn text-slate-300 bg-slate-400">{item}</button></li>
-              {/each}
-            </ul>
-          </div>
-        </div>
-      </div>
-
-    </div>
-
-    <div class="basis-1/2 border-2 border-pink-300">
-      Test 1
-    </div>
-    
-  </div>
-
-  
+    <TokenField bind:isDropdownOpen={isDropdownOpen1} bind:menuItems={menuItems1}  />
+    <TokenField bind:isDropdownOpen={isDropdownOpen1} bind:menuItems={menuItems2}  />
 
   <div class="basis-1/2 border-2 border-pink-300">
       Test 2
---
+++ web/src/routes/TokenFIeld.svelte
@@ -0,0 +1,66 @@
+<script>
+  import MdFormatListBulleted from 'svelte-icons/md/MdFormatListBulleted.svelte'
+  import MdKeyboardArrowDown from 'svelte-icons/md/MdKeyboardArrowDown.svelte'
+
+  export let isDropdownOpen
+  export let menuItems
+
+  export const handleDropdownClick = () => {
+    isDropdownOpen = !isDropdownOpen 
+  }
+
+  export const handleDropdownFocusLoss = ({ relatedTarget, currentTarget }) => {
+    if (relatedTarget instanceof HTMLElement && currentTarget.contains(relatedTarget)) return 
+    isDropdownOpen = false
+  }
+</script>
+
+
+
+<div class="flex flex-col">
+    <div class="flex flex-row justify-between h-8 bg-slate-400">
+      <div class="mx-2">
+        Amount to be added
+      </div>
+      <div class="mx-2">
+        Balance
+      </div>
+    </div>
+
+    <div class="flex flex-row justify-between h-12 px-4 bg-slate-400">
+      <form method="POST">
+        <label>
+          <input
+            name="description"
+            autocomplete="off"
+            class="text-slate-700"
+          />
+        </label>
+      </form>
+
+      <button class="bg-slate-400 hover:bg-slate-600 rounded-md border-2 border-pink-300">
+        Max
+      </button>
+
+      <div class="flex justify-between items-center rounded-md border-2 border-pink-300">
+        <div class="rounded-md">
+          Select Token
+        </div>
+        <div class="dropdown" on:focusout={handleDropdownFocusLoss}>
+          <button class="btn top-2 bg-slate-400 px-1 py-1 rounded-md border-0 border-pink-300" on:click={handleDropdownClick} >
+            {#if isDropdownOpen}
+              <MdKeyboardArrowDown />
+            {:else}
+              <MdFormatListBulleted/>
+            {/if}
+          </button>
+          <ul class="dropdown-content menu bg-slate-400 w-44" style:visibility={isDropdownOpen ? 'visible' : 'hidden'}>
+            {#each menuItems as item}
+              <li><button class="btn text-slate-300 bg-slate-400">{item}</button></li>
+            {/each}
+          </ul>
+        </div>
+      </div>
+    </div>
+
+  </div>
